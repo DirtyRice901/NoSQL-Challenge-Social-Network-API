20b8ec1775945e63498ffe6a8a3304a49ccc88b3
Kevin Rice <reatriniti@gmail.com>
2023-10-30 18:12:36 -0400

    completed server.js and connections.js

--- server.js
+++ server.js
@@ -1,60 +1,23 @@
 ////////////// import express for routing /////////////////////////////////////////////////////////////////////////////////////////////////
 const express = require('express');
+const db = require('./config/connection'); 
+const routes = require('./routes');
 
-////////////// run npm install mongod and require mongodb and MongoClient ///////////////////////////////////////////////////////////////
-const {MongoClient} = require('mongodb');
+////////////// establish enrironment variables /////////////////////////////////////////////////////////////////////////////////////////////
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-////////////// route to the database ////////////////////////////////////////////////////////////////////////////////////////////////////
-const app = express();  
-const port = 3001; // port number
+////////////// establish middleware /////////////////////////////////////////////////////////////////////////////////////////////
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // parse incoming JSON data //
 
-////////////// connection string to local instance of MongoDB //////////////////////////////////////////////////////////////////////////////////////////////////
-const connectionString = 'mongodb://localhost:27017'; // default port for MongoDB
+////////////// establish routes /////////////////////////////////////////////////////////////////////////////////////////////
+app.use(routes);
 
-////////////// initialize a new instance of MongoClient /////////////////////////////////////////////////////////////////////////////////////////////
-const client = new MongoClient(connectionString);
-
-////////////// declare a variable to hold the database /////////////////////////////////////////////////////////////////////////////////////////////
-let db;
-
-////////////// create variable to hold my database name
-const dbName = 'socialMediaDB';
-
-////////////// use connect method to connect to the mongoDB server /////////////////////////////////////////////////////////////////////////////////////////////
-client.connect()
-    .then(() => {
-        console.log('Connected to MongoDB server');
-        // assign the database to the db variable //
-        db = client.db(dbName); 
-
-        // start the express server //
-        app.listen(port, () => {
-            console.log(`Server started on port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err.message);
+////////////// establish mongoose connection /////////////////////////////////////////////////////////////////////////////////////////////
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
     });
+});
 
-////////////// Built in Express function that parses incoming requests with JSON payloads /////////////////////////////////////////////////////////////////////////////////////////////
-app.use(express.json());
-
-app.post('/user', (req, res) => {
-    // Use db connection to insert a new user into the database //
-    db.collection('user').insertOne({ 
-        name: req.body.name,
-        email: req.body.email,
-        thoughts: [],
-        friends: [],
-
-    })
-    .then((result) => {
-        console.log(result);
-        res.status(200).send('User created');
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Internal server error');
-    });
-}
-);
