64adda1f40bc77b918a98c5fc8097f172a48b6a7
Kevin Rice <reatriniti@gmail.com>
2023-10-31 20:07:38 -0400

    corrected mongoose variable to connect to compass line 4

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socialMediaDB", {
-mongoose.connect( 'mongodb://localhost/socialMediaDB');
+mongoose.connect( 'mongodb://localhost:27017/socialMediaDB');
 
 ////////////// export the connection /////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = mongoose.connection;
