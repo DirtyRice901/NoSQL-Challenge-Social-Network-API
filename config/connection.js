450effecc893c1b1192bdabc4ebde8dfe42bfc4a
Kevin Rice <reatriniti@gmail.com>
2023-10-31 20:12:04 -0400

    added new parser and topology engine to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socialMediaDB", {
-mongoose.connect( 'mongodb://localhost:27017/socialMediaDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialMediaDB', {
+    useNewUrlParser: true, // use the new parser
+    useUnifiedTopology: true, // use the new topology engine
+    
+}  );
+
 
 ////////////// export the connection /////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = mongoose.connection;
