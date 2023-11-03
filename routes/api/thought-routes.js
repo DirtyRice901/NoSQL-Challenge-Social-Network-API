f1065d7a7ef1d9790e5cb6ba43d39b941d57bd13
Kevin Rice <reatriniti@gmail.com>
2023-11-03 17:22:54 -0400

    fixed undefined variables in ../api/thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,20 +2,20 @@ const router = require('express').Router();
 ////////////////// import all of the API routes from /api/index.js //////////////
 const {
     getAllThoughts, // get all thoughts
-    getThoughtById, // get one thought by id
+    getThoughtsById, // get one thought by id
     createThought, // create a thought
-    updateThoughtById,  // update a thought by id
+    updateThoughtByID,  // update a thought by id
     deleteThought, // delete a thought by id
-    addReaction,  // add a reaction to a thought
+    createReaction,  // add a reaction to a thought
     deleteReaction  // delete a reaction from a thought
  ////////////// import functions from the thought-controller //////////////    
 } = require('../../controllers/thought-controller'); 
 
 router.route('/').get(getAllThoughts).post(createThought); // /api/thoughts
 
-router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThought); // /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtsById).put(updateThoughtByID).delete(deleteThought); // /api/thoughts/:thoughtId
 
-router.route('/:thoughtId/reactions').post(addReaction); // /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction); // /api/thoughts/:thoughtId/reactions
 
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction); // /api/thoughts/:thoughtId/reactions/:reactionId
 
